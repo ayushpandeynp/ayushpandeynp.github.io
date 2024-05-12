874f104bc53cc958efaac19045970efea9a05471
Ayush Pandey <ap6178@nyu.edu>
2024-05-12 15:48:34 +0400

    reorganization

---
+++ catstronaut/README.md
@@ -0,0 +1,24 @@
+# Catstronaut: A Space Odyssey
+This story is a cat-themed space exploration adventure, depicted through an interactive comic website. The protagonist is a cat who embarks on a journey to the moon.
+
+## Story
+Our protagonist, a cat, looks up at the moon every night, wondering what lies far beyond its earthly home. One day, the cat finds an opportunity to become an astronaut and, without a second thought, seizes this chance. It undergoes training and prepares to board a rocket. Finally, the cat travels about 238,000 miles to reach the moon, where it looks up and sees the Earth from a new perspective.
+
+## Process
+The project was a collaborative effort among our team members, each contributing their unique skills to bring the concept to life:
+
+### Comic Design
+Ian sketched the comics based on our class discussions, using a vibrant color palette to ensure that the panels matched the website's overall aesthetics.
+
+### Sound
+To craft the soundtrack for this comic, Stefan laid down a foundational chord progression in a major key, capturing the lively and spirited essence of cats. He then layered arcade-inspired synth leads and bass to infuse the composition with a science fiction ambiance, employing the distinctive timbres of digital instruments. The track is designed to evoke a sense of excitement and exploration, reflecting the adventurous spirit of the narrative.
+
+### Website
+Ayush focused on the website design, ensuring that the space theme complemented the narrative of the comic story. He implemented interactive elements to reveal the comic panels sequentially as users scroll down, enhancing the storytelling experience.
+
+## Reflection
+Reflecting on the project, we successfully achieved our goal of creating an engaging and immersive experience. The collaboration between team members allowed us to blend different artistic elements seamlessly, resulting in a cohesive final product. The interactive website design, in particular, played a crucial role in bringing the story to life, allowing users to feel like they were part of the cat's journey through space. Overall, the project has met our expectations, and we love it!
+
+## External Sources
+1. Space Background - https://www.wallpaperflare.com/astro-photography-astronomy-long-exposure-night-sky-space-wallpaper-gcpml
+2. Infinity & Beyond Sound - https://www.youtube.com/watch?v=az0hjPtImrg&ab_channel=GamingSoundFX
---
+++ catstronaut/js/main.js
@@ -0,0 +1,108 @@
+// total viewport height
+let viewportHeight = window.innerHeight;
+
+// "Enter" Button click listener
+const button = document.querySelector("button");
+const buttonClickSound = document.getElementById("Infinity");
+
+// background music
+const backgroundMusic = document.getElementById("ComicScore");
+backgroundMusic.loop = true;
+backgroundMusic.volume = 0.3;
+
+button.addEventListener("click", function () {
+  buttonClickSound.play();
+  setTimeout(() => {
+    backgroundMusic.play();
+  });
+  window.scrollTo(0, viewportHeight + 1);
+});
+
+// scroll event listener
+window.addEventListener("scroll", function () {
+  // current viewport height (in case of resizing the window)
+  viewportHeight = window.innerHeight;
+
+  // this is the total scroll position
+  const scrollPosition = window.scrollY;
+
+  // first 3 panels
+  const first_3 = [
+    document.querySelector(".one"),
+    document.querySelector(".two"),
+    document.querySelector(".three"),
+  ];
+
+  // middle rocket panel
+  const mid = document.querySelector(".four");
+
+  // last 2 panels
+  const last_2 = [
+    document.querySelector(".five"),
+    document.querySelector(".six"),
+  ];
+
+  // for the first 3 panels
+  if (
+    scrollPosition > viewportHeight &&
+    scrollPosition < viewportHeight * 2.3
+  ) {
+    first_3[0].classList.add("active");
+
+    if (scrollPosition > viewportHeight * 1.1) {
+      first_3[1].classList.add("active");
+    }
+
+    if (scrollPosition > viewportHeight * 1.3) {
+      first_3[2].classList.add("active");
+    }
+
+    first_3.forEach(function (img) {
+      img.style.transform = `translateY(${scrollPosition - viewportHeight}px)`;
+    });
+  } else {
+    first_3.forEach(function (img) {
+      img.classList.remove("active");
+    });
+  }
+
+  // middle rocket panel
+  if (
+    scrollPosition > viewportHeight * 2.3 &&
+    scrollPosition < viewportHeight * 3.7
+  ) {
+    mid.classList.add("active");
+    let animationStartPosition =
+      document.querySelector("section:nth-child(3)").offsetTop +
+      viewportHeight / 3;
+    let speed = 0.5;
+
+    mid.style.transform = `translateY(${
+      animationStartPosition - speed * scrollPosition
+    }px)`;
+  } else {
+    mid.classList.remove("active");
+  }
+
+  // for the last 2 panels
+  if (
+    scrollPosition > viewportHeight * 3.7 &&
+    scrollPosition < viewportHeight * 4.8
+  ) {
+    last_2[0].classList.add("active");
+
+    if (scrollPosition > viewportHeight * 4) {
+      last_2[1].classList.add("active");
+    }
+
+    last_2.forEach((img) => {
+      img.style.transform = `translateY(${
+        scrollPosition - viewportHeight * 3.6
+      }px)`;
+    });
+  } else {
+    last_2.forEach((img) => {
+      img.classList.remove("active");
+    });
+  }
+});
